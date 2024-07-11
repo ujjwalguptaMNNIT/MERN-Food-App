dcf34fc5b13fe1f42992ae408bb8b58b54b003fe
Ujjwal Gupta <143928527+ujjwalguptaMNNIT@users.noreply.github.com>
2024-07-11 10:55:47 +0530

    Add files via upload

---
+++ Backend/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import reservationRouter from "./route/reservationRoute.js";
+import { errorMiddleware } from "./error/error.js";
+import { dbConnection } from "./database/dbConnection.js";
+import { Reservation } from "./models/reservationSchema.js";
+
+const app = express();
+dotenv.config({path :"./config/config.env"});
+
+app.use(
+    cors({
+      origin: [process.env.FRONTEND_URL],
+      methods: ["POST"],
+      credentials: true,
+    })
+);
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1/reservation", reservationRouter);
+
+
+dbConnection();
+
+app.use(errorMiddleware);
+
+export default app;
